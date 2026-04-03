3db09aeb3d81a3fd76d80e97908dd3604c83b53d
Eirren <edv2019@gmail.com>
2026-04-03 09:01:26 -0700

    Added #include for functions, starting

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <list>
+#include <set>
+using namespace std;
+
+int main() {
+    
+
+    return 0;
+}
+
+/* syntax examples:
+auto start = high_resolution_clock::now()
+auto end = high_resolution_clock::now()
+auto duration = duration_cast<milliseconds>(end - start)
+duration.count() references elapsed milliseconds
+*/
