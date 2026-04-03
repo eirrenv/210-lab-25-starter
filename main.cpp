7de1a1332e42779caefb5668a74aa4ec6dffdcf0
Eirren <edv2019@gmail.com>
2026-04-03 09:23:08 -0700

    Set up read timers and test prints for function speeds

--- main.cpp
+++ main.cpp
@@ -8,23 +8,38 @@ using namespace std;
 using namespace std::chrono;
 
 int main() {
-    auto start = high_resolution_clock::now();
+    
 
     string codeToInsert;
+    vector<string> data;
     vector<string> codeVector;
     list<string> codeList;
     set<string> codeSet;
     ifstream fin("codes.txt");
     while (fin >> codeToInsert) {
-        codeVector.push_back(codeToInsert);
-        codeList.push_back(codeToInsert);
-        codeSet.insert(codeToInsert);
+        data.push_back(codeToInsert);
     }
+    // vector reading time
+    auto start = high_resolution_clock::now();
+    for (const auto &x : data) codeVector.push_back(x);
     auto end = high_resolution_clock::now();
+    auto vectorSpeed = duration_cast<nanoseconds>(end - start);
 
-    auto duration = duration_cast<nanoseconds>(end - start);
+    // list reading time
+    start = high_resolution_clock::now();
+    for (const auto &x : data) codeList.push_back(x);
+    end = high_resolution_clock::now();
+    auto listSpeed = duration_cast<nanoseconds>(end - start);
 
-    cout << "Time taken: " << duration.count() << " nanoseconds" << endl;
+    // set reading time
+    start = high_resolution_clock::now();
+    for (const auto &x : data) codeSet.insert(x);
+    end = high_resolution_clock::now();
+    auto setSpeed = duration_cast<nanoseconds>(end - start);
+
+    cout << "Vector speed: " << vectorSpeed.count() << " nanoseconds" << endl;
+    cout << "List speed: " << listSpeed.count() << " nanoseconds" << endl;
+    cout << "Set speed: " << setSpeed.count() << " nanoseconds" << endl;
 
     
 
