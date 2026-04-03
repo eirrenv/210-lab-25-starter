c2951e54c5e14d8b5bc1dd2ce3c411e87d4b3c9e
Eirren <edv2019@gmail.com>
2026-04-03 09:13:45 -0700

    Added basic code for inserting

--- main.cpp
+++ main.cpp
@@ -3,10 +3,31 @@
 #include <vector>
 #include <list>
 #include <set>
+#include <fstream>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
+    auto start = high_resolution_clock::now();
+
+    string codeToInsert;
+    vector<string> codeVector;
+    list<string> codeList;
+    set<string> codeSet;
+    ifstream fin("codes.txt");
+    while (fin >> codeToInsert) {
+        codeVector.push_back(codeToInsert);
+        codeList.push_back(codeToInsert);
+        codeSet.insert(codeToInsert);
+    }
+    auto end = high_resolution_clock::now();
+
+    auto duration = duration_cast<nanoseconds>(end - start);
+
+    cout << "Time taken: " << duration.count() << " nanoseconds" << endl;
+
     
+
 
     return 0;
 }
