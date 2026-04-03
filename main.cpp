2000c7139732bf0383e650a11e5371ac5ea1747b
Eirren <edv2019@gmail.com>
2026-04-03 09:28:28 -0700

    Added sort speeds

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <set>
 #include <fstream>
+#include <algorithm> // vector sort :)
 using namespace std;
 using namespace std::chrono;
 
@@ -23,23 +24,43 @@ int main() {
     auto start = high_resolution_clock::now();
     for (const auto &x : data) codeVector.push_back(x);
     auto end = high_resolution_clock::now();
-    auto vectorSpeed = duration_cast<nanoseconds>(end - start);
+    auto vectorSpeed = duration_cast<milliseconds>(end - start);
 
     // list reading time
     start = high_resolution_clock::now();
     for (const auto &x : data) codeList.push_back(x);
     end = high_resolution_clock::now();
-    auto listSpeed = duration_cast<nanoseconds>(end - start);
+    auto listSpeed = duration_cast<milliseconds>(end - start);
 
     // set reading time
     start = high_resolution_clock::now();
     for (const auto &x : data) codeSet.insert(x);
     end = high_resolution_clock::now();
-    auto setSpeed = duration_cast<nanoseconds>(end - start);
+    auto setSpeed = duration_cast<milliseconds>(end - start);
 
-    cout << "Vector speed: " << vectorSpeed.count() << " nanoseconds" << endl;
-    cout << "List speed: " << listSpeed.count() << " nanoseconds" << endl;
-    cout << "Set speed: " << setSpeed.count() << " nanoseconds" << endl;
+    // test speed prints
+    cout << "Reading Speed:" << endl;
+    cout << "Vector speed: " << vectorSpeed.count() << " milliseconds" << endl;
+    cout << "List speed: " << listSpeed.count() << " milliseconds" << endl;
+    cout << "Set speed: " << setSpeed.count() << " milliseconds" << endl;
+
+    // vector sorting time
+    start = high_resolution_clock::now();
+    sort(codeVector.begin(), codeVector.end());
+    end = high_resolution_clock::now();
+    vectorSpeed = duration_cast<milliseconds>(end - start);
+
+    // list sorting time
+    start = high_resolution_clock::now();
+    codeList.sort();
+    end = high_resolution_clock::now();
+    listSpeed = duration_cast<milliseconds>(end - start);
+
+    // test sort prints, set already sorted
+    cout << "Sorting speed:" << endl;
+    cout << "Vector speed: " << vectorSpeed.count() << " milliseconds" << endl;
+    cout << "List speed: " << listSpeed.count() << " milliseconds" << endl;
+    cout << "Set speed: " << "-1" << " milliseconds" << endl;
 
     
 
